c671fc246081fe444c279ea4bc14974e02822264
Carson Streufert <nexusdesignsfeedback@gmail.com>
2016-04-11 18:23:32 -0400

    Added creating work record feature

--- app/controllers/work_records_controller.rb
+++ app/controllers/work_records_controller.rb
@@ -7,4 +7,18 @@ class WorkRecordsController < ApplicationController
     @work_record = WorkRecord.find params[:id]
     @lines = @work_record.work_record_lines
   end
+
+  def new
+    @work_record = WorkRecord.new
+  end
+
+  def create
+    work_record_params = params.require(:work_record).permit(:date)
+    @work_record = WorkRecord.new(work_record_params)
+    if @work_record.save
+      redirect_to @work_record
+    else
+      render "new"
+    end
+  end
 end
